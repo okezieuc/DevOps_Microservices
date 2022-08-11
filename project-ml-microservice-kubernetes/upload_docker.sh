11e970bac69989d04821bc6ccab2ea3a30d3851e
okezieuc <c.okezie.u@gmail.com>
2022-08-11 20:57:47 +0000

    build: Update docker and kubernetes commands

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=okezieuc/ml-model-microservice
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag ml-model-microservice okezieuc/ml-model-microservice:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push okezieuc/ml-model-microservice:latest
+
